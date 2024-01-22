6e0d99f660bea576095a7529bc1a5322402c69a2
shihabdider <shihabdider@gmail.com>
2024-01-22 09:15:12 -0500

    test: propgating changes

--- src/components/PdfHighlighter.tsx
+++ src/components/PdfHighlighter.tsx
@@ -416,6 +416,7 @@ export class PdfHighlighter<T_HT extends IHighlight> extends PureComponent<
   };
 
   onSelectionChange = () => {
+    console.log('testing selection change');
     const container = this.containerNode;
     const selection = getWindow(container).getSelection();
 
@@ -449,7 +450,6 @@ export class PdfHighlighter<T_HT extends IHighlight> extends PureComponent<
   onScroll = () => {
     const { onScrollChange } = this.props;
 
-    console.log('testing on scroll');
     onScrollChange();
 
     this.setState(
